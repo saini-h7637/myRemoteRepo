b07c07ebd5fb57a4951bef686921391785a047ab
saini-h7637 <sainih6@student.douglascollege.ca>
2020-06-26 22:19:32 -0700

    Brach B1 chages

--- src/myJavaProject/myAppTester.java
+++ src/myJavaProject/myAppTester.java
@@ -18,7 +18,7 @@ public class myAppTester {
 	  System.out.println();
 
 	  // add a line below to call your myTestMethod from here  then remove the block comment symbols  
-
+	  myTestMethod(n);
 	}
 	
 	//This is a method to reverse the number by recursion
@@ -34,14 +34,28 @@ public class myAppTester {
 	   }
 
 
-	   /* 
-	       add your test method in this space then remove the block comment symbols
-	       public static void myTestMethod(...){
-
-            
+	      // add your test method in this space then remove the block comment symbols
+	      // My Test Method
+	       public static void myTestMethod(int n){
+            if(n < 10)
+            	System.out.println("Sum of the Digit of the number is: " + n);
+            else {
+            	
+            	 int len = Integer.toString(n).length();
+            	 int sum = 0;
+            	 int n1 = 0;
+            	 while(len!= 0) {
+            		 n1 = n % 10 ;
+            		 n = n / 10 ;
+            		 sum = sum + n1;
+            		 len--;
+            	 }
+            	 System.out.println("Sum of the Digit of the number is: " + sum);
+            }
+           
 
 		   }
 	 
-	   */
+	   
 
 }
